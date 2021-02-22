e0feccf7baeced40929943fcd5015817b00530a6
Florent Pastor <flopastor06@gmail.com>
2021-02-22 20:22:49 +0100

    backup

--- lib/clock.dart
+++ lib/clock.dart
@@ -5,20 +5,29 @@ import 'package:http/http.dart' as http;
 
 class SimpleClock extends StatefulWidget {
   final String url;
-  final String text;
+  final String fromImage;
+  final String toImage;
+  final String fromText;
+  final String toText;
 
-  SimpleClock(this.text, this.url);
+  SimpleClock(
+      this.url, this.fromImage, this.toImage, this.fromText, this.toText);
 
   @override
-  State<StatefulWidget> createState() => _SimpleClockState(text, url);
+  State<StatefulWidget> createState() =>
+      _SimpleClockState(url, fromImage, toImage, fromText, toText);
 }
 
 class _SimpleClockState extends State<SimpleClock> {
   Future<int> _futureResult;
   final String url;
-  final String title;
+  final String fromImage;
+  final String toImage;
+  final String fromText;
+  final String toText;
 
-  _SimpleClockState(this.title, this.url);
+  _SimpleClockState(
+      this.url, this.fromImage, this.toImage, this.fromText, this.toText);
 
   @override
   void initState() {
@@ -40,7 +49,7 @@ class _SimpleClockState extends State<SimpleClock> {
   Text createText(String text) => Text(text,
       textDirection: TextDirection.ltr,
       style: TextStyle(
-        fontSize: 30,
+        fontSize: 24,
         fontFamily: "Helvetica",
         fontWeight: FontWeight.w300,
       ));
@@ -48,24 +57,70 @@ class _SimpleClockState extends State<SimpleClock> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            createText(title),
-            FutureBuilder<int>(
-              future: _futureResult,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return createText(snapshot.data.toString());
-                } else if (snapshot.hasError) {
-                  return createText("${snapshot.error}");
-                }
-                return CircularProgressIndicator();
-              },
-            )
-          ],
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Image.asset(fromImage,
+                        width: MediaQuery.of(context).size.width / 4),
+                    decoration: BoxDecoration(border: Border.all()),
+                  ),
+
+                  Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(fromText,
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontFamily: "Helvetica",
+                            fontWeight: FontWeight.w300,
+                          )))
+                ],
+              )),
+              Icon(Icons.arrow_right, color: Colors.black, size: 48.0),
+              Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Image.asset(toImage,
+                        width: MediaQuery.of(context).size.width / 4),
+                    decoration: BoxDecoration(border: Border.all()),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(toText,
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontFamily: "Helvetica",
+                            fontWeight: FontWeight.w300,
+                          )))
+                ],
+              ))
+            ],
+          ),
+          Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: FutureBuilder<int>(
+                future: _futureResult,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return createText(snapshot.data.toString() + " min");
+                  } else if (snapshot.hasError) {
+                    return createText("- min");
+                  }
+                  return CircularProgressIndicator();
+                },
+              ))
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
-
 import 'clock.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseAdMob.instance.initialize(appId: "ca-app-pub-8141904202528647~9452198431");
+  InterstitialAd myInterstitial = InterstitialAd(
+    adUnitId: "ca-app-pub-8141904202528647/2990376645",
+    listener: (MobileAdEvent event) {
+      print("InterstitialAd event is $event");
+    },
+  );
+
+  myInterstitial
+    ..load()
+    ..show(
+      anchorType: AnchorType.bottom,
+      anchorOffset: 0.0,
+      horizontalCenterOffset: 0.0,
+    );
+
+  runApp(MyApp());
+}
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -10,7 +30,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightBlue,
       ),
       home: MyHomePage(title: 'Tende Limone Timer'),
     );
@@ -33,13 +53,67 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Center(
-            child: Column(
+            child: SingleChildScrollView(
+                child: Column(
           children: <Widget>[
-            SimpleClock("Italy -> France",
-                'https://tunnel-server.florentpastor.com/italy'),
-            SimpleClock("France -> Italy",
-                'https://tunnel-server.florentpastor.com/france'),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    leading: Icon(Icons.error_outline, color: Colors.orange),
+                    contentPadding: const EdgeInsets.all(15.0),
+                    title: Text('Weather conditions'),
+                    subtitle: Text(
+                        'The remaning time computation is based on image analysis. Please understand that the values could be wrong in case of bad weather conditions.'),
+                  )
+                ],
+              ),
+            ),
+            Card(
+                child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: SimpleClock(
+                        'https://tunnel-server.florentpastor.com/italy',
+                        'assets/images/italy.png',
+                        'assets/images/france.png',
+                        "ITA",
+                        "FRA")),
+              ],
+            )),
+            Card(
+                child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: SimpleClock(
+                        'https://tunnel-server.florentpastor.com/france',
+                        'assets/images/france.png',
+                        'assets/images/italy.png',
+                        "FRA",
+                        "ITA")),
+              ],
+            )),
+//            Card(
+//              child: Column(
+//                mainAxisSize: MainAxisSize.min,
+//                children: <Widget>[
+//                  Padding(
+//                    padding: const EdgeInsets.all(20.0),
+//                    child: Image.network(
+//                      'https://picsum.photos/250?image=9',
+//                    ),
+//                  )
+//                ],
+//              ),
+//            )
           ],
-        )));
+        ))
+    )
+    );
   }
 }
